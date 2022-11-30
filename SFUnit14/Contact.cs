65c7950d580af8e2e92367e08d217c08c1b0832f
Morgul3000 <shokuave3000@gmail.com>
2022-11-30 11:56:03 +0300

    Add project files.

---
+++ SFUnit14/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFUnit14
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long Phone { get; set; }
+        public string EMail { get; set; }
+        public Contact(string name, string surname, long phone, string eMail)
+        {
+            Name = name;
+            Surname = surname;
+            Phone = phone;
+            EMail = eMail;
+        }
+
+    }
+}
---
+++ SFUnit14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SFUnit14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contact> phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            List<Contact> sortPhoneBook = phoneBook.OrderBy(x => x.Name)
+                                         .ThenBy(x => x.Surname)
+                                         .ToList();
+
+            foreach (Contact con in sortPhoneBook)
+            {
+                Console.WriteLine($"Имя - {con.Name}, фамилия - {con.Surname}, телефон - {con.Phone}, e-Mail - {con.EMail}");
+            }
+
+        }
+    }
+}
